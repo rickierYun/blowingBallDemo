b986d1bdcf3e8dc9dbeea34e21339d345f45a031
ouyangyunhui <ouyangyunhui@ouyangyunhuideMacBook-Pro.local>
2015-09-11 12:21:21 +0800

    reference changed

---
+++ collisonView/labelViewController.swift
@@ -0,0 +1,76 @@
+//
+//  labelViewController.swift
+//  collisonView
+//
+//  Created by 欧阳云慧 on 15/9/11.
+//  Copyright (c) 2015年 欧阳云慧. All rights reserved.
+//
+
+import UIKit
+
+class labelViewController: UIViewController,HolderViewDelegate {
+
+    var holderView = HolderView(frame: CGRectZero)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        addHolderView()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func addHolderView() {
+        let boxSize: CGFloat = 100.0
+        holderView.frame = CGRect(x: view.bounds.width / 2 - boxSize / 2,
+            y: view.bounds.height / 2 - boxSize / 2,
+            width: boxSize,
+            height: boxSize)
+        holderView.parentFrame = view.frame
+        holderView.delegate = self
+        view.addSubview(holderView)
+        holderView.addOval()
+    }
+    
+    func animateLabel() {
+        // 1
+        holderView.removeFromSuperview()
+        view.backgroundColor = Colors.blue
+        
+        // 2
+        var label: UILabel = UILabel(frame: view.frame)
+        label.textColor = Colors.white
+        label.font = UIFont(name: "HelveticaNeue-Thin", size: 170.0)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "F"
+        label.transform = CGAffineTransformScale(label.transform, 0.25, 0.25)
+        view.addSubview(label)
+        
+        // 3
+        UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: ({
+                label.transform = CGAffineTransformScale(label.transform, 4.0, 4.0)
+            }), completion: { finished in
+                self.addButton()
+        })
+    }
+    
+    func addButton() {
+        let button = UIButton()
+        button.frame = CGRectMake(0.0, 0.0, view.bounds.width, view.bounds.height)
+        button.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
+        view.addSubview(button)
+    }
+    
+    func buttonPressed(sender: UIButton!) {
+        view.backgroundColor = Colors.white
+        view.subviews.map({ $0.removeFromSuperview() })
+        holderView = HolderView(frame: CGRectZero)
+        addHolderView()
+    }
+}
