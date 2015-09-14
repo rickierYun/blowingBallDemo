6b3461930cda26b558fc66c186af255007e41d8b
ouyangyunhui <ouyangyunhui@ouyangyunhuideMacBook-Pro.local>
2015-09-14 13:43:18 +0800

    Add a new animation  when  open the app. 添加一个开机的动画

--- collisonView/loginViewController.swift
+++ collisonView/loginViewController.swift
@@ -8,17 +8,68 @@
 
 import UIKit
 
-class loginViewController: UIViewController, UITextFieldDelegate {
+class loginViewController: UIViewController, UITextFieldDelegate,HolderViewDelegate {
     
     @IBOutlet weak var name: UITextField! { didSet { name.delegate = self}}
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var holderView: HolderView!
     
     var imageArry = NSMutableArray()
+    var holderViewFrame = HolderView(frame: CGRectZero)
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder() // show the keyboard first
         return true
     }
+    
+ //MARK: addHolderView 
+ //添加开始动画
+    func addHolderView() {
+        let boxSize: CGFloat = 100.0
+        holderViewFrame.frame = CGRect(x: holderView.bounds.width / 2 - boxSize / 2,
+            y: holderView.bounds.height / 2 - boxSize / 2,
+            width: boxSize,
+            height: boxSize)
+        holderViewFrame.parentFrame = holderView.frame
+        holderViewFrame.delegate = self
+        holderView.addSubview(holderViewFrame)
+        holderViewFrame.addOval()
+    }
+
+//MARK: animateLabel
+//添加文字动画
+    func animateLabel() {
+        holderViewFrame.removeFromSuperview()
+        holderView.backgroundColor = Colors.blue
+        
+        var label: UILabel = UILabel(frame: holderView.frame)
+        label.textColor = Colors.white
+        label.font = UIFont(name: "Party LET", size: 70.0)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "Blowing"
+        label.transform = CGAffineTransformScale(label.transform, 0.25, 0.25)
+        holderView.addSubview(label)
+        
+        UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1, options: UIViewAnimationOptions.CurveEaseInOut,
+            animations: ({
+                label.transform = CGAffineTransformScale(label.transform, 4.0, 4.0)
+            }), completion: { finished in
+                self.addButton()
+        })
+    }
+    
+    func addButton() {
+        let button = UIButton()
+        button.frame = CGRectMake(0.0, 0.0, holderView.bounds.width, holderView.bounds.height)
+        button.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
+        holderView.addSubview(button)
+    }
+    
+    func buttonPressed(sender: UIButton!) {
+        holderView.removeFromSuperview()
+    }
+
+
 //MARK: transmitNameToTitile
 //将写好的名字传送游戏界面的title
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -43,6 +94,11 @@ class loginViewController: UIViewController, UITextFieldDelegate {
         imageView.animationImages = imageArry as [AnyObject]
         imageView.animationDuration = 2
         imageView.startAnimating()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        addHolderView()
     }
     
 }
