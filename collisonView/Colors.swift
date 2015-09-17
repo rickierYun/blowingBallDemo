afef8c6c121238ed25db6c87e65c0c2a142a5ede
ouyangyunhui <ouyangyunhui@ouyangyunhuideMacBook-Pro.local>
2015-09-17 10:03:05 +0800

    recommit color file

---
+++ collisonView/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  SBLoader
+//
+//  Created by 欧阳云慧 on 15/9/11.
+//  Copyright (c) 2015年 欧阳云慧. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+    static let blue = UIColor(red: 46.0 / 255.0, green: 117.0 / 255.0, blue: 146.0 / 255.0, alpha: 1.0)
+    static let red = UIColor(red: 209.0 / 255.0, green: 80.0 / 255.0, blue: 50.0 / 255.0, alpha: 1.0)
+    static let green = UIColor(red: 30.0 / 255.0, green: 180.0 / 255.0, blue: 100.0 / 255.0, alpha: 1.0)
+    static let white = UIColor.whiteColor()
+    static let clear = UIColor.clearColor()
+}
